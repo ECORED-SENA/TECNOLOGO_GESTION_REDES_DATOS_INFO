ea3873cb277a3bb58689d160fe3e6794b1df8605
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-06-25 14:53:38 -0500

    Cargue inicial

---
+++ src/views/Inicio.vue
@@ -0,0 +1,207 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p Bienvenido a la Tecnología en gestión de redes de datos.
+      br
+      br
+      | En este programa de formación aprenderás a configurar y gestionar servicios de red en #[strong servidores locales y en la nube], equipos activos de interconexión, infraestructura de voz sobre IP y #[strong redes inalámbricas], e implementación y monitoreo de #[strong políticas de seguridad de la información], todas las tecnologías necesarias para la #[strong creación y soporte de infraestructura de TI] que esté al servicio de los objetivos de #[strong negocio del sector productivo], incrementando su nivel de #[strong competitividad y productividad] requerido en el entorno globalizado actual.
+      br
+      br
+      | Esta tecnología tendrá una duración de #[strong 27 meses] y se impartirá en modalidad #[strong 100% virtual].
+      br
+      br
+      | Gracias por hacer parte de los miles de colombianos que le apuestan al cambio. 
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación<br>del programa
+
+    p.mb-0 La constante evolución y desarrollo tecnológico unido a la mayor adopción de las TIC en las empresas para su fortalecimiento y desarrollo, ha generado un creciente potencial productivo para la Gestión de redes y sistemas informáticos, que gracias al incremento de la demanda de sistemas de comunicación e intercambio de información requeridos por la industria, la adopción, desarrollo de políticas y normativas por parte del estado, ha identificado la necesidad de potenciar el área TIC como elemento fundamental para buscar una sociedad equitativa, un sector productivo competitivo y unos ciudadanos con niveles de formación contextualizada. El fortalecimiento y crecimiento socioeconómico tanto a nivel regional como nacional dependen en gran medida de contar con un recurso humano calificado, capaz de responder integralmente a la dinámica empresarial del sector TI (Tecnología informática).
+      br
+      br
+      | El programa en gestión de redes de datos ofrece a los aprendices formación en configuración y gestión de servicios de red en servidores locales y en la nube, equipos activos de interconexión, infraestructura de voz sobre IP y redes inalámbricas, e implementación y monitoreo de políticas de seguridad de la información, todas tecnologías necesarias para la creación y soporte de infraestructura de TI que esté al servicio de los objetivos de negocio del sector productivo, incrementando su nivel de competitividad y productividad requerido en el entorno globalizado actual.
+      
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Competencias a<br>desarrollar
+
+    ul.fa-ul
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Administrar hardware y software de seguridad en la red a partir de normas internacionales.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Administrar infraestructura tecnológica de red según modelos de referencia y procedimiento técnico.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Aplicación de conocimientos de las ciencias naturales de acuerdo con situaciones del contexto productivo y social.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo de acuerdo con las políticas organizacionales y la normatividad vigente.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Configurar dispositivos activos de interconexión según especificaciones del diseño y protocolos técnicos.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Configurar dispositivos de cómputo de acuerdo con especificaciones del diseño y protocolos técnicos. 
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Desarrollar procesos de comunicación eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal y productivo.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Ejercer derechos fundamentales del trabajo en el marco de la constitución política y los convenios internacionales. 
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Enrique Low Murtra - Interactuar en el contexto productivo y social de acuerdo con principios éticos para la construcción de una cultura de paz. 
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Generar hábitos saludables de vida mediante la aplicación de programas de actividad física en los contextos productivos y sociales. 
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Gestionar procesos propios de la cultura emprendedora y empresarial de acuerdo con el perfil personal y los requerimientos de los contextos productivo y social.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Implementar red inalámbrica local según especificaciones del diseño y estándares técnicos.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Implementar tecnologías de voz sobre IP de acuerdo con el diseño y normas técnicas.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Interactuar en lengua inglesa de forma oral y escrita dentro de contextos sociales y laborales según los criterios establecidos por el marco común europeo de referencia para las lenguas.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Orientar investigación formativa según referentes técnicos. 
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales.
+
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de información.
+    
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Pefil de <br>ingreso
+
+    p #[strong Nivel académico:] <br> Media académica
+      br
+      br
+      | #[strong Curso:] <br> Undécimo
+      br
+      br
+      | #[strong Formación para el trabajo y el desarrollo humano:] <br> No.
+      br
+      br
+      | #[strong Número de horas:] <br> No.
+      br
+      br
+      | #[strong Requiere experiencia laboral:] <br> No.
+      br
+      br
+      | #[strong Edad mínima definida en la ley:] <br> 14 años.
+      br
+      br
+      | #[strong Requisitos adicionales:]<br> Título de bachiller o su equivalente validación aprobada por la entidad competente. Superar prueba de aptitud, motivación, interés y competencias mínimas de ingreso.
+      br
+      br
+      | #[strong Restricciones de ingreso soportadas en la legislación vigente:] <br> No existe legislación que establezca restricciones de ingreso a nivel de discapacidad física y cognitiva. 
+      br
+      br
+      | #[strong Aspectos actitudinales, motivacionales y de interés:]  <br> Capacidad de automotivación, autocontrol y autogestión. Actitud positiva, frente al entorno y la interacción social. Solucionador de problemas, receptivo y con vocación a los procesos de servicios de tecnología.
+    
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Pefil de <br>egreso
+
+    p Sus egresados serán competentes en labores de configuración de equipos activos y servicios de red en redes de datos; configuración de redes inalámbricas locales; implementación sistemas de VoIP; implementación de políticas de seguridad en redes de datos, detección de vulnerabilidades de seguridad en redes de datos, solución de problemas de seguridad en redes de datos, monitoreo de redes de datos, gestión de entrega de servicios, realización de copias de respaldo de configuración de equipos e información, elaboración de informes de gestión de red entre otras. 
+    br
+    br
+    | Los egresados de la Tecnología en gestión de redes de datos podrán ser líderes de los procesos laborales de configuración de equipos activos, seguridad y gestión de redes de datos cableadas e inalámbricas en proyectos. Se desempeñará principalmente en empresas del sector de tecnologías de la información (TI) en el área de redes de datos. Igualmente, estará en capacidad de crear su propia empresa prestadora de servicios de redes de datos. El egresado será una persona íntegra, con actitudes y aptitudes de trabajo, cuyos valores éticos estarán siempre presentes y dispuestos para mejorar su proyecto de vida y la calidad del servicio.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 6. Estrategia<br>metodológica
+
+    p.mb-4 Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias. 
+      br
+      br
+      | Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el que hacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:      
+
+    ul.fa-ul
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El instructor - tutor
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El entorno
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Las TIC
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El trabajo colaborativo   
+    
+
+
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+  
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.resultados-aprendizaje
+  &__item
+    margin-bottom: 20px
+    &:last-child
+      margin-bottom: 0
+
+.desarrollo-contenidos
+  &__item
+    display: flex
+    align-items: center
+    justify-content: space-between
+    margin-bottom: 25px
+    padding: 5px
+    border-radius: $base-border-radius
+
+    &__texto
+      display: flex
+      align-items: center
+
+    &__icono
+      display: block
+      background-color: $color-sistema-g
+      font-size: 20px
+      font-weight: $base-black-font-weight
+      text-align: center
+      padding: 10px
+      line-height: 1em
+      width: 40px
+      margin-right: 15px
+
+    &:last-child
+      margin-bottom: 0
+
+    &:hover
+      background-color: $color-sistema-g
+      font-weight: $base-black-font-weight
+      color: $color-sistema-a
+</style>
